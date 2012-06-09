0d6bb65c4f47e98f3a845859ae04a9b454c6bf34
Aaron McLeod <sircoolguy99@gmail.com>
2012-06-09 17:33:24 -0400

    Added creating, editing, updating and deleting tickets to assigning permissions interface

--- app/helpers/admin/permissions_helper.rb
+++ app/helpers/admin/permissions_helper.rb
@@ -1,7 +1,10 @@
 module Admin::PermissionsHelper
   def permissions
     {
-      "view" => "View"
+      "view" => "View",
+      "create tickets" => "Create Tickets",
+      "edit tickets" => "Edit tickets",
+      "delete tickets" => "Delete tickets"
     }
   end
 end
